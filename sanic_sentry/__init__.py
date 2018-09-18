108e10048249eb7badde896c6db3a0dc9b8c5300
Eran Kampf <eran@ekampf.com>
2018-09-17 21:42:57 -0700

    Handle exc_info is (None, None, None)

--- sanic_sentry/__init__.py
+++ sanic_sentry/__init__.py
@@ -22,7 +22,7 @@ class SanicSentryErrorHandler(ErrorHandler):
     def default(self, request, exception):
         if not isinstance(exception, self.exceptions_to_ignore):
             exc_info = sys.exc_info()
-            if not exc_info:
+            if not exc_info or all((not x for x in exc_info)):
                 exc_info = (type(exception), exception, exception.__traceback__)
 
             extra = dict()
